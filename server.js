d1c81e6c335a71cf98b123aee0fa9e6f4740be13
adamtlee <adam.lee346@gmail.com>
2018-07-25 21:00:59 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// The port number that the server runs on
+const port = (process.env.PORT || 3000);
+
+// Add body parsing for Content-Type json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(port, () => {
+    console.log(`player app running on ${port}`)
+});
